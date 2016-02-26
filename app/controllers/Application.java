5bc53605e242bbb2c5c87b992c24c2485b1ec475
Rafael Sampaio <rafael.tavares@ccc.ufcg.edu.br>
2016-02-26 16:56:47 -0300

    Adiciona bootstrap, cria tela de cadastro, edita routes

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -8,7 +8,11 @@ import views.html.*;
 public class Application extends Controller {
 
     public Result index() {
-        return ok(index.render("Your new application is ready."));
+        return ok(index.render("Novo Tasdasdatulo asdgyuas"));
+    }
+
+    public Result cadastro() {
+        return ok(cadastro.render());
     }
 
 }
