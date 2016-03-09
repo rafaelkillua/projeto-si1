d480a7ce6e819ad41bc120563cae2f03c74b3abb
Rafael Sampaio <rafael.tavares@ccc.ufcg.edu.br>
2016-03-09 12:25:19 -0300

    Cadastro estilizado e botão limpar funcional, Login estilizado, Main teve sua navbar alterada e botões renomeados, Javascript (hello) com funções, css ajeitado

--- public/javascripts/hello.js
+++ public/javascripts/hello.js
@@ -1,3 +1,11 @@
 if (window.console) {
   console.log("Welcome to your Play application's JavaScript!");
 }
+
+function ativarQtde() {
+  document.getElementById("qtde").removeAttribute("disabled");
+}
+
+function desativarQtde() {
+  document.getElementById("qtde").setAttribute("disabled", "disabled");
+}
