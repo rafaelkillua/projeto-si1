1be3c93b545794878ded7a2cfed432b3dc027b70
Rafael Sampaio <rafael.tavares@ccc.ufcg.edu.br>
2016-03-11 17:32:32 -0300

    Tela de solicitações. alterações em routes, main e Application.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -15,5 +15,7 @@ public class Application extends Controller {
 
     public Result agendarCarona(){return ok(agendarCarona.render()); }
 
-   public Result enderecos() {return ok(enderecos.render()); }
+    public Result enderecos() {return ok(enderecos.render()); }
+
+    public Result solicitacoes() {return ok(solicitacoes.render()); }
 }
