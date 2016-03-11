8f1dfcc52529854318890415eed107fb07dff99f
Rafael Sampaio <rafael.tavares@ccc.ufcg.edu.br>
2016-03-11 16:07:51 -0300

    Cadastro atualizado, novo placeholder para imagens, funções em JS, login estilizado, barra do main atualizada

--- public/javascripts/hello.js
+++ public/javascripts/hello.js
@@ -1,11 +1,31 @@
 if (window.console) {
-  console.log("Welcome to your Play application's JavaScript!");
+    console.log("Welcome to your Play application's JavaScript!");
 }
 
 function ativarQtde() {
-  document.getElementById("qtde").removeAttribute("disabled");
+    document.getElementById("qtde").removeAttribute("disabled");
 }
 
 function desativarQtde() {
-  document.getElementById("qtde").setAttribute("disabled", "disabled");
+    document.getElementById("qtde").setAttribute("disabled", "disabled");
+}
+
+function ativarEndereco() {
+    document.getElementById("endretorno").removeAttribute("disabled");
+}
+
+function desativarEndereco() {
+    document.getElementById("endretorno").setAttribute("disabled", "disabled");
+}
+
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            document.getElementById("preview").setAttribute("src", String(e.target.result));
+        }
+
+        reader.readAsDataURL(input.files[0]);
+    }
 }
