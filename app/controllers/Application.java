135ae59e424d1f2d1d0cc1accbc75659da8f056b
Rafael Sampaio <rafael.tavares@ccc.ufcg.edu.br>
2016-03-11 14:32:24 -0300

    Index atualizado, rota nova criada, método criado no application

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -7,11 +7,11 @@ import views.html.*;
 
 public class Application extends Controller {
 
-    public Result index() { return ok(index.render("Index teste")); }
+    public Result index() { return ok(index.render()); }
 
-    public Result login() {
-        return ok(login.render());
-    }
+    public Result login() { return ok(login.render()); }
 
     public Result cadastro() {return ok(cadastro.render()); }
+
+    public Result enderecos() {return ok(enderecos.render()); }
 }
