c20c434f418aa8260344214716af1ba19eeff664
Rafael Sampaio <rafael.tavares@ccc.ufcg.edu.br>
2016-03-11 22:46:14 -0300

    Agendar Carona, Enderecos e Index estilizados, Login com meia funcionalidade, design do main alterado, perfil criado, rotas ajeitadas, hello com mais funções

--- public/javascripts/hello.js
+++ public/javascripts/hello.js
@@ -29,3 +29,46 @@ function readURL(input) {
         reader.readAsDataURL(input.files[0]);
     }
 }
+
+function toggleVisMain() {
+    var e = document.getElementById("mainDiv");
+    e.style.display = (e.style.display == "block") ? "none" : "block";
+}
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + "; " + expires;
+}
+
+function deleteCookie(cname) {
+    var d = new Date();
+    d.setTime(d.getTime() - (365 * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=; " + expires;
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1);
+        if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
+    }
+    return "";
+}
+
+function checkCookie() {
+    var username = getCookie("username");
+    if (username == "") {
+        location.href = "/login";
+        setCookie("username", "deslogado", 365);
+    }
+}
+
+function logar() {
+    setCookie("username", document.getElementById("nome").value, 365);
+    location.href = "/";
+}
