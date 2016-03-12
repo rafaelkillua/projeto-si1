c91b1acf5e5d653514a95fe8ae5d075f8cf38cda
Rafael Sampaio <rafael.tavares@ccc.ufcg.edu.br>
2016-03-11 23:54:04 -0300

    Botões ajeitados em algumas classes, main ajeitado, login feito (por cima), funções criadas em JS

--- public/javascripts/hello.js
+++ public/javascripts/hello.js
@@ -2,6 +2,7 @@ if (window.console) {
     console.log("Welcome to your Play application's JavaScript!");
 }
 
+// ativar e desativar campos de texto
 function ativarQtde() {
     document.getElementById("qtde").removeAttribute("disabled");
 }
@@ -18,6 +19,34 @@ function desativarEndereco() {
     document.getElementById("endretorno").setAttribute("disabled", "disabled");
 }
 
+// mostrar e esconder o main
+function esconderMain() {
+    document.getElementById("mainDiv").setAttribute("class", "container hidden");
+}
+
+function mostrarMain() {
+    document.getElementById("mainDiv").setAttribute("class", "container visible");
+}
+
+// aceitar, recusar, ignorar e solicitar caronas
+function aceitarSolicitacao() {
+    alert("Carona aceita. Telefone do passageiro: (83) 9 9876-5432.");
+}
+
+function recusarSolicitacao() {
+    alert("Carona ignorada.");
+}
+
+function solicitarCarona() {
+    alert("Carona solicitada.")
+}
+
+function ignorarCarona() {
+    alert("Carona ignorada.")
+}
+
+
+// mostrar imagem no cadastro
 function readURL(input) {
     if (input.files && input.files[0]) {
         var reader = new FileReader();
@@ -30,11 +59,8 @@ function readURL(input) {
     }
 }
 
-function toggleVisMain() {
-    var e = document.getElementById("mainDiv");
-    e.style.display = (e.style.display == "block") ? "none" : "block";
-}
 
+// brincar com cookies
 function setCookie(cname, cvalue, exdays) {
     var d = new Date();
     d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
@@ -62,13 +88,27 @@ function getCookie(cname) {
 
 function checkCookie() {
     var username = getCookie("username");
+    var visibility = getCookie("visibility");
     if (username == "") {
         location.href = "/login";
         setCookie("username", "deslogado", 365);
+        setCookie("visibility", "false", 365);
+    }
+    if (visibility == "false") {
+        esconderMain();
+    } else if (visibility == "true") {
+        mostrarMain();
     }
 }
 
+//logar e deslogar do sistema
 function logar() {
     setCookie("username", document.getElementById("nome").value, 365);
+    setCookie("visibility", "true", 365);
     location.href = "/";
+}
+
+function deslogar() {
+    deleteCookie("username");
+    deleteCookie("visibility");
 }
