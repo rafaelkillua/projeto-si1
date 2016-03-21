80f64546bcd4f10b06ac4964e007b05ed1247a8e
Rafael Sampaio <rafael.tavares@ccc.ufcg.edu.br>
2016-03-21 03:29:30 -0300

    Muitas mudanças para se adequar às novidades

---
+++ app/models/Usuario.java
@@ -0,0 +1,16 @@
+package models;
+
+/**
+ * Created by Rafael on 20/03/2016.
+ */
+public class Usuario {
+    public String nome;
+
+    public Usuario(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+}
